4def0d21ae2dbf0634fab381ab0e65019583e3e6
swap123 <misalswapnil77@gmail.com>
2022-03-22 14:00:56 +0530

    Using all Three Methods

--- LinkedList.java
+++ LinkedList.java
@@ -4,9 +4,9 @@ public class LinkedList {
     public static void main(String[] args) {
         SingleLinkedList singleLinkedList = new SingleLinkedList();
 
-        singleLinkedList.insertFirst(70);
-        singleLinkedList.insertFirst(30);
         singleLinkedList.insertFirst(56);
+        singleLinkedList.insertAt (1,30);
+        singleLinkedList.insertLast(70);
 
 
         singleLinkedList.printNodeElement();
